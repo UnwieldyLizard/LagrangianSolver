2f6683e12a6323151fd4fd2622eb0dbb1c543456
UnwieldyLizard <unwieldylizard@gmail.com>
2023-06-05 20:10:54 -0700

    reorganization

--- lagrangian_solver.py
+++ lagrangian_solver.py
@@ -10,7 +10,7 @@ class Solver():
         self.name = name
     
     def initialize_sim(self, lagrangian, initial_state, metric, granularity):
-        self.dims = initial_state.shape[2]
+        self.dims = np.array(initial_state).shape[2]
         self.lagrangian = lagrangian
         self.metric = metric
         self.positions = np.array(initial_state)[1, :]
@@ -431,31 +431,4 @@ def kerr_metric(position):
             metric[i,j] = nu[i,j]-f*k[i]*k[j]
     return metric
 
-sol = Solver("Schwarzschild_4D")
-#sol = Solver("cool_orbit")
-sol.initialize_postprocess(rel_lagrangian, schwarzschild_metric, [10000,0.001])
-#initial_cond = np.array([[[0.1  , 5   , 0    ], [0.1  , 20  , 0  ]]
-#                        ,[[0.6  , 5.05, 0.14 ], [0.6  , 20  , 0.055]]])
-#working schwarzschild_4D
-#initial_cond = np.array([[[0.1  , 5   , 0    ,0], [0.1  , 20  , 0    ,0]]
-#                        ,[[0.6  , 5.05, -0.098 ,+0.098], [0.6  , 20  , 0.04, 0.04]]])
-#initial_cond = np.array([[[0.1  , 21  , 0    ,0]]
-#                        ,[[0.6  , 21  , 0.04, 0.04]]])
-#initial_cond = np.array([[[0.1  , 0  , 0    ,25]]
-#                        ,[[0.6  , 0.04  , 0.04, 25]]])
-#r=20
-#dt = 0.1
-#v = np.sqrt(0.5/r)
-#initial_cond = np.array([[[0.1  , r+20 , 0    ,0]]
-#                        ,[[0.1+dt , r+20  , v*dt, 0]]])
-#initial_cond = np.array([[[0.0  , 5    , 0   ], [0.0  , 20  , 0    ]]
-#                        ,[[0.5 , 5.05, 0.14 ], [0.2 , 20  , 0]]])
-#initial_cond = np.array([[[0.0, 0.0, 0.9, 0]],
-#                         [[0.01, 0.003, 0.899, 0]]])
-#sol.initialize_sim(rel_lagrangian, initial_cond, kerr_metric, [100000,0.0001,1e-6])
-#sol.full_sim()
-sol.open_old()
-#sol.rename("kerr_1particle")
-#sol.peg(fps=48)
-#sol.plot(orientation=[30, 220])
-sol.plot_diagnostic()
+
---
+++ main.py
@@ -0,0 +1,30 @@
+from lagrangian_solver import *
+
+sol = Solver("Schwarzschild_4D")
+#sol = Solver("cool_orbit")
+sol.initialize_postprocess(rel_lagrangian, schwarzschild_metric, [10000,0.001])
+#initial_cond = [[[0.1  , 5   , 0    ], [0.1  , 20  , 0  ]]
+#               ,[[0.6  , 5.05, 0.14 ], [0.6  , 20  , 0.055]]]
+#working schwarzschild_4D
+#initial_cond = [[[0.1  , 5   , 0    ,0], [0.1  , 20  , 0    ,0]]
+#               ,[[0.6  , 5.05, -0.098 ,+0.098], [0.6  , 20  , 0.04, 0.04]]]
+#initial_cond = [[[0.1  , 21  , 0    ,0]]
+#               ,[[0.6  , 21  , 0.04, 0.04]]]
+initial_cond = [[[0.1  , 0  , 0    ,25]]
+               ,[[0.6  , 0.04  , 0.04, 25]]]
+#r=20
+#dt = 0.1
+#v = np.sqrt(0.5/r)
+#initial_cond = [[[0.1  , r+20 , 0    ,0]]
+#               ,[[0.1+dt , r+20  , v*dt, 0]]]
+#initial_cond = [[[0.0  , 5    , 0   ], [0.0  , 20  , 0    ]]
+#               ,[[0.5 , 5.05, 0.14 ], [0.2 , 20  , 0]]]
+#initial_cond = [[[0.0, 0.0, 0.9, 0]],
+#                [[0.01, 0.003, 0.899, 0]]]
+sol.initialize_sim(rel_lagrangian, initial_cond, kerr_metric, [10000,0.0001,1e-6])
+sol.full_sim()
+#sol.open_old()
+#sol.rename("kerr_1particle")
+#sol.peg(fps=48)
+#sol.plot(orientation=[30, 220])
+#sol.plot_diagnostic()
